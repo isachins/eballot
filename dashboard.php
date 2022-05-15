bdb486192adf2682b26d26b3ef2b75d30da04477
iamsachinsharma <sachinsharma2168@gmail.com>
2022-05-15 18:54:07 +0530

    commit

--- dashboard.php
+++ dashboard.php
@@ -295,24 +295,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
                     <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
                         Dashboard
                     </h2>
-                    <div class="flex items-center py-2 overflow-y-auto whitespace-nowrap">
-
-                        <a href="#" class="text-gray-600 dark:text-gray-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                            </svg>
-                        </a>
-
-                        <span class="mx-5 text-gray-500 dark:text-gray-300">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-
-                        <a href="#" class="text-gray-600 dark:text-gray-200 hover:underline">
-                            Account
-                        </a>
-                    </div>
+                    
 
 
 
@@ -395,10 +378,8 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
 
                     <!-- Charts -->
-                    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-                        Charts
-                    </h2>
-                    <div class="grid gap-6 mb-8 md:grid-cols-20 ">
+                   
+                    <div class="grid gap-6 mb-8 md:grid-cols-2 ">
                         <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
                             <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
                                 Result
